37ed5f507028aef08042641b88a418999a59e8a5
Влад <trembling.of.the.strings@gmail.com>
2021-08-14 19:13:36 +0500

    1.0v, added group system for posts

---
+++ yatube/posts/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
+
+
+def index(request):
+    template = 'posts/index.html'
+    posts = Post.objects.all()[:10]
+    context = {
+        'posts': posts,
+    }
+    return render(request, template, context)
+
+
+def group_posts(request, slug):
+    template = 'posts/group_list.html'
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, template, context)
+    
