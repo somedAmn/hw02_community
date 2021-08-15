70a887f2340b557cfb7baa5e99e173b7d0158a01
Влад <trembling.of.the.strings@gmail.com>
2021-08-15 15:59:50 +0500

    fix template, models, import style

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,6 +1,5 @@
-from django.db import models
 from django.contrib.auth import get_user_model
-
+from django.db import models
 
 User = get_user_model()
 
@@ -24,9 +23,9 @@ class Post(models.Model):
     )
     group = models.ForeignKey(
         Group,
-        on_delete=models.CASCADE,
+        on_delete=models.SET_NULL,
         blank=True,
-        null=True,
+        null=True
     )
 
     class Meta:
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Post, Group
+from django.shortcuts import get_object_or_404, render
+
+from .models import Group, Post
 
 
 def index(request):
@@ -14,7 +15,7 @@ def index(request):
 def group_posts(request, slug):
     template = 'posts/group_list.html'
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    posts = Post.objects.filter(group=group).all()[:10]
     context = {
         'group': group,
         'posts': posts,
